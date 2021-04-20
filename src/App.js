ed4aa21b335f5de8e1a8af78bc540189162e44b0
nakao1043 <nakao1043@yahoo.co.jp>
2021-04-20 19:42:21 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API, Storage } from 'aws-amplify';
+import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
 
@@ -19,10 +19,6 @@ function App() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
     await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
       return note;
     }))
     setNotes(apiData.data.listNotes.items);
@@ -31,26 +27,37 @@ function App() {
   async function createNote() {
     if (!formData.name || !formData.description) return;
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
-    }
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
 
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  function changeNoteName(newNote, newName){
+    notes.forEach(note => {
+      if(note === newNote){
+        note.name = newName;
+      } 
+    });
+    setNotes(notes);
   }
 
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchNotes();
+  function changeNoteDescription(newNote, newDsc){
+    notes.forEach(note => {
+      if(note === newNote){
+        note.description = newDsc;
+      } 
+    });
+    setNotes(notes);
+  }
+
+  async function updateNote(note) {
+    await API.graphql({ query: updateNoteMutation, variables: { input: {id:note.id, name: note.name, description: note.description}  }});
+    setNotes(notes);
+  }
+
+  async function deleteNote( delNote ) {
+    const newNotesArray = notes.filter(note => note.id !== delNote.id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id:delNote.id } }});
   }
 
   return (
@@ -66,21 +73,26 @@ function App() {
         placeholder="Note description"
         value={formData.description}
       />
-      <input
-        type="file"
-        onChange={onChange}
-      />
       <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
         {
           notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              {
-                note.image && <img src={note.image} style={{width: 400}} />
-              }
-              <br/>
+            <div key={note.id || note.name} style={{marginBottom: 20}}>
+              <h2>
+                name:&nbsp;
+                <input
+                  onChange = {e => changeNoteName(note, e.target.value)}
+                  defaultValue={note.name}
+                />
+              </h2>
+              <p>
+                description:&nbsp;
+                <input
+                  onChange = {e => changeNoteDescription(note, e.target.value)}
+                  defaultValue={note.description}
+                />
+              </p>
+              <button style={{marginRight: 10}} onClick={() => updateNote(note)}>Update note</button>
               <button onClick={() => deleteNote(note)}>Delete note</button>
             </div>
           ))
--- src/index.js
+++ src/index.js
@@ -1,106 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { API } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation, updateNote as updateNoteMutation } from './graphql/mutations';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
-const initialFormState = { name: '', description: '' }
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-function App() {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
-
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
-
-  function changeNoteName(newNote, newName){
-    notes.forEach(note => {
-      if(note === newNote){
-        note.name = newName;
-      } 
-    });
-    setNotes(notes);
-  }
-
-  function changeNoteDescription(newNote, newDsc){
-    notes.forEach(note => {
-      if(note === newNote){
-        note.description = newDsc;
-      } 
-    });
-    setNotes(notes);
-  }
-
-  async function updateNote(note) {
-    await API.graphql({ query: updateNoteMutation, variables: { input: {id:note.id, name: note.name, description: note.description}  }});
-    setNotes(notes);
-  }
-
-  async function deleteNote( delNote ) {
-    const newNotesArray = notes.filter(note => note.id !== delNote.id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id:delNote.id } }});
-  }
-
-  return (
-    <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name} style={{marginBottom: 20}}>
-              <h2>
-                name:&nbsp;
-                <input
-                  onChange = {e => changeNoteName(note, e.target.value)}
-                  defaultValue={note.name}
-                />
-              </h2>
-              <p>
-                description:&nbsp;
-                <input
-                  onChange = {e => changeNoteDescription(note, e.target.value)}
-                  defaultValue={note.description}
-                />
-              </p>
-              <button style={{marginRight: 10}} onClick={() => updateNote(note)}>Update note</button>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-            </div>
-          ))
-        }
-      </div>
-      <AmplifySignOut />
-    </div>
-  );
-}
-
-export default withAuthenticator(App);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
